0513479b5465df7730ca6a480e9a94dc9c85947f
ken <wejk@outlook.com>
2024-07-13 19:22:04 +1000

    Add solution file and maui project

---
+++ PassportPhoto/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PassportPhoto;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PassportPhoto/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PassportPhoto;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
